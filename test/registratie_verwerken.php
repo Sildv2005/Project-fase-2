57c6bd78c46369ea899ae73aca3bca4604099b5f
ElenaBulut <206010@student.aventus.nl>
2025-01-13 12:54:41 +0100

    AAAAAA

--- test/registratie_verwerken.php
+++ test/registratie_verwerken.php
@@ -53,34 +53,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "Registratie succesvol! Je kunt nu inloggen.";
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/style.css" rel="stylesheet" type="text/css"/>
-    <title>Registreren</title>
-</head>
-<body>
-    <nav>
-        <img src="images/logo_small.png" alt="Coral Yachts">
-        <a href="index.html">Home</a>
-        <a href="zoekjacht.html">Reserveren</a>
-        <a href="contact.html">Contact</a>
-        <a href="overons.html">Over ons</a>
-        <a href="klantinlog.html">Inloggen</a>
-    </nav>
-
-    <div class="register-container">
-        <h1>Registreren</h1>
-        <form action="register.php" method="POST">
-            <input type="text" name="naam" placeholder="Je naam" required>
-            <input type="email" name="email" placeholder="E-mailadres" required>
-            <input type="password" name="wachtwoord" placeholder="Wachtwoord" required>
-            <button type="submit">Registreren</button>
-        </form>
-        <p>Heb je al een account? <a href="klantinlog.html">Inloggen</a></p>
-    </div>
-</body>
-</html>
